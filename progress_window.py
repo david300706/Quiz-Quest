a883cad8b8e53783885d9c77dcf2e46addefeb07
example <itamardego12@gmail.com>
2024-09-18 15:20:53 +0300

    progres_window

--- progress_window.py
+++ progress_window.py
@@ -1,21 +1,17 @@
 # import progress_screen
 import pygame.event
 
+import progress_consts
+
+screen = pygame.display.set_mode((progress_consts.WINDOW_WIDTH, progress_consts.WINDOW_HEIGHT))
+
 state = {"is_window_open": True}
+
 
 def main():
     pygame.init()
 
     while state["is_window_open"]:
-
-
-
-
-
-
-
-
-
 
 
 def user_events():
@@ -27,11 +23,23 @@ def user_events():
                 state["enter_game"] = True
 
 
-def movement():
-    pass
+def solider_moving_right(soldier_position):
+    soldier_position = list(soldier_position)
+    soldier_position[1] += progress_consts.DISTANCE
+    return tuple(soldier_position)
 
 
+def draw_massage(massage, font_size, text_color, location):
+    font = pygame.font.SysFont(progress_consts.FONT_NAME, font_size, bold=True)
+    text_img = font.render(massage, True, text_color)
+    text_width = text_img.get_width()
+    text_height = text_img.get_height()
+    location_x = location[0] - text_width / 2
+    location_y = location[1] - text_height / 2
+    screen.blit(text_img, (location_x, location_y))
 
 
-
-
+def pop_up_window(solider_position):
+    if solider_position[1] == progress_consts.DISTANCE:
+        draw_massage(progress_consts.STUDY_INFO[0], progress_consts.POP_WINDOW_FONT_SIZE, "black",
+                     progress_consts.POP_WINDOW_FONT_LOCATION)
