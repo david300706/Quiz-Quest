edbf4fc5899e84573774c32efdeaedb7c1c5a090
git1 <oded564738291@gmail>
2024-09-18 12:21:02 +0300

    added all function defintion to the screen_maze.py , added to consts.py the WINDOW_WIDTH, WINDOW_HEIGHT , and intial the main maze display

--- screen_maze.py
+++ screen_maze.py
@@ -14,11 +14,19 @@ def draw_white_cube():
     pass
 
 
-def draw_grid():
+def draw_grid(matrix):
+    """
+    draw the maze on the display_surface
+    :param matrix: a 2d matrix of 1 for no pass , 0 for pass or 2 for question mark
+    """
     pass
 
 
 def question_mark():
+    """
+
+    :return:
+    """
     pass
 
 
