e83391de641b1cb199a2723f847e386d541942b8
example <itamardego12@gmail.com>
2024-09-18 12:11:22 +0300

    ajhj

--- inputs.py
+++ inputs.py
@@ -2,7 +2,7 @@ def main_input():
     pass
 def main_maze_input():
     pass
-def question_input():
+def question_input(): # TODO: input validation check
     question_list = []
 
 
@@ -11,7 +11,7 @@ def question_input():
     print('Please enter the answers you wish to have in your multiple choice question')
 
     for i in range(1,5):
-        answer_option = input(f'please Enter answer number {i} for your multiple choice question ')
+        answer_option = input(f'please Enter answer number {i} for your multiple choice question: ')
         question_list.append(answer_option)
     correct_answer_num = int(input('please Enter the number of the correct answer: '))
     question_list.append(correct_answer_num - 1)
