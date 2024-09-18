db44586d03f938d018efab5fdb96c3a02968dced
git1 <oded564738291@gmail>
2024-09-18 12:17:02 +0300

    added all function defintion to the screen_maze.py , added to consts.py the WINDOW_WIDTH, WINDOW_HEIGHT , and intial the main maze display

---
+++ screen_maze.py
@@ -0,0 +1,34 @@
+import pygame
+from consts import WINDOW_WIDTH, WINDOW_HEIGHT
+
+# general setup
+pygame.init()
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+
+def draw_black_cube():
+    pass
+
+
+def draw_white_cube():
+    pass
+
+
+def draw_grid():
+    pass
+
+
+def question_mark():
+    pass
+
+
+def draw_massage():
+    pass
+
+
+def draw_player():
+    pass
+
+
+def draw_win():
+    pass
