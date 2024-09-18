68fb445058519a12996621ce5b32994f9d798fe8
example <itamardego12@gmail.com>
2024-09-18 11:39:48 +0300

    initial inputs

---
+++ inputs.py
@@ -0,0 +1,13 @@
+def main_input():
+    pass
+def main_maze_input():
+    pass
+def question_input():
+    count = 0
+    done = False
+    while not done:
+        count += 1
+        question = input(f"Hi teacher, please enter question number {count} - If you're done, enter done:").lower()
+
+
+
