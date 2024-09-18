5960f0609a77b6b0773c7b9c69dfa41ff19e3efe
example <itamardego12@gmail.com>
2024-09-18 16:35:28 +0300

    progres_window

--- progress_window.py
+++ progress_window.py
@@ -44,11 +44,27 @@ def solider_moving_right(soldier_position):
 
 
 
-def pop_up_window(solider_position):
-    if solider_position[1] == progress_consts.DISTANCE:
-        progress_screen.draw_massage(progress_consts.STUDY_INFO[0], progress_consts.POP_WINDOW_FONT_SIZE, "black",
-                     progress_consts.POP_WINDOW_FONT_LOCATION)
 
 
 # state["soldier_location"] = solider_moving_right(state["soldier_location"])
 main()
+# def solider_moving_right(soldier_position):
+#     soldier_position = list(soldier_position)
+#     soldier_position[1] += progress_consts.DISTANCE
+#     return tuple(soldier_position)
+#
+#
+# def draw_massage(massage, font_size, text_color, location):
+#     font = pygame.font.SysFont(progress_consts.FONT_NAME, font_size, bold=True)
+#     text_img = font.render(massage, True, text_color)
+#     text_width = text_img.get_width()
+#     text_height = text_img.get_height()
+#     location_x = location[0] - text_width / 2
+#     location_y = location[1] - text_height / 2
+#     screen.blit(text_img, (location_x, location_y))
+#
+#
+# def pop_up_window(solider_position):
+#     if solider_position[1] == progress_consts.DISTANCE:
+#         draw_massage(progress_consts.STUDY_INFO[0], progress_consts.POP_WINDOW_FONT_SIZE, "black",
+#                      progress_consts.POP_WINDOW_FONT_LOCATION)
