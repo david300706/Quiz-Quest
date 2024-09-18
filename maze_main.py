be4274040f78ff68587fcf8cc91ffcbcba903b3c
liel <lielkassa12@gmail.com>
2024-09-18 14:01:06 +0300

    state init

--- maze_main.py
+++ maze_main.py
@@ -1,4 +1,7 @@
 import consts
+import consts
+import screen
+import pygame
 
 
 def create_maze_grid():
@@ -34,3 +37,44 @@ def convert_index_to_cords(index_x, index_y):
 
 for x in create_maze_grid():
     print(x)
+
+state = {"player_location": [2, 2],
+         "game_running": True}
+
+
+def main():
+    pygame.init()
+    user_events()
+    while state["game_running"]:
+        user_events()
+
+
+def user_events():
+    pygame.init()
+    global state
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+
+        if event.type == pygame.KEYDOWN:
+
+            if event.key == pygame.K_UP:
+                print(state["player_location"])
+                state["player_location"][1] -= 1
+
+            if event.key == pygame.K_DOWN:
+                print(state["player_location"])
+                state["player_location"][1] += 1
+
+            if event.key == pygame.K_RIGHT:
+                print(state["player_location"])
+                state["player_location"][0] += 1
+
+            if event.key == pygame.K_LEFT:
+                print(state["player_location"])
+                state["player_location"][0] -= 1
+
+
+user_events()
+
+main()
