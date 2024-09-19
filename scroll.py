c56e09f1bcc5e8b7632d72f5f998d00648dbc934
git1 <oded564738291@gmail>
2024-09-19 08:36:09 +0300

    addeig doce

--- scroll.py
+++ scroll.py
@@ -2,4 +2,10 @@ from consts import WINDOW_WIDTH, WINDOW_HEIGHT
 
 
 def draw_scroll(scroll, display_surface, coordinates):
+    """
+    draw a scroll on the display_surface
+    :param scroll: the image of scroll
+    :param display_surface: the display_surface to draw on
+    :param coordinates: x and y coordinates NOT index , if wou want to use index use the function convert_index_to_cords(index_x, index_y)
+    """
     display_surface.blit(scroll, coordinates)
